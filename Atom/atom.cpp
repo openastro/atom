d3f6d889cf7f570d2fb4318c6f3e8cc09e3ca7eb
Kartik Kumar <me@kartikkumar.com>
2014-07-25 11:57:39 +0200

    Add project CMakeLists.txt and atom source file.

---
+++ Atom/atom.cpp
@@ -0,0 +1,14 @@
+/*    
+ * Copyright (c) 2010-2014, Delft University of Technology
+ * Copyright (c) 2010-2014, K. Kumar (me@kartikkumar.com)
+ * All rights reserved.
+ * See http://bit.ly/12SHPLR for license details.
+ */
+
+#include <cstdlib>
+
+//! Execute main-function.
+int main( const int numberOfInputs, const char* inputArguments[ ] )
+{
+	return EXIT_SUCCESS;
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ATOM
 ===================
 
-The `ATOM` (Accurate Transfer Orbit Model) is a library that offers a SGP4/SDP4-based analytical solver, to rapidly compute high-thrust orbital transfers.
+`ATOM` (Accurate Transfer Orbit Model) is a library that offers a SGP4/SDP4-based analytical solver, to rapidly compute high-thrust orbital transfers.
