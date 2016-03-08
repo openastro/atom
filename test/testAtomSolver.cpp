ca921fe343d66b244a77b06d987d64fe72b830e8
Abhishek Agrawal <abhishek.agrawal@protonmail.com>
2016-03-08 16:56:34 +0100

    Add test case for atom solver

--- test/testAtomSolver.cpp
+++ test/testAtomSolver.cpp
@@ -25,6 +25,64 @@ typedef double Real;
 typedef std::vector< Real > Vector3;
 typedef std::pair< Vector3, Vector3 > Velocities;
 
+TEST_CASE( "Execute Atom Solver", "[atom-solver]" )
+{
+    // Set departure position [km].
+    Vector3 departurePosition( 3 );
+    departurePosition[ 0 ] = 385.246482619633;
+    departurePosition[ 1 ] = 2243.00298468343;
+    departurePosition[ 2 ] = 6723.68851012157;
+
+    // Set departure velocity [km/s].
+    Vector3 departureVelocity( 3 );
+    departureVelocity[ 0 ] = 2.38102833809095;
+    departureVelocity[ 1 ] = -6.83464321936554;
+    departureVelocity[ 2 ] = 2.02594519285685;
+
+    // Set arrival position [km].
+    Vector3 arrivalPosition( 3 );
+    arrivalPosition[ 0 ] = 385.246482619633;
+    arrivalPosition[ 1 ] = 2243.00298468343;
+    arrivalPosition[ 2 ] = 6723.68851012157;
+
+    // Set arrival velocity [km/s].
+    Vector3 arrivalVelocity( 3 );
+    arrivalVelocity[ 0 ] = -2.39479831205517;
+    arrivalVelocity[ 1 ] = 6.3102097995122;
+    arrivalVelocity[ 2 ] = -3.09055920479061;
+
+    // Set departure epoch.
+    DateTime departureEpoch( 2016, 1, 11, 4, 53, 22 );
+
+    // Time-of-flight [s].
+    const Real timeOfFlight = 2830.0;
+
+    SECTION( "Test case with no iterations" )
+    {
+        // Execute Atom solver to compute departure and arrival velocities that bridge the given
+        // positions.
+        std::string dummyString = "";
+        int numberOfIterations = 0;
+        const Velocities velocities = executeAtomSolver( departurePosition,
+                                                         departureEpoch,
+                                                         arrivalPosition,
+                                                         timeOfFlight,
+                                                         departureVelocity,
+                                                         dummyString,
+                                                         numberOfIterations );
+
+        // Check that departure and arrival velocities match results.
+        for ( int i = 0; i < 3; i++ )
+        {
+            REQUIRE( departureVelocity[ i ] == Approx( velocities.first[ i ] ).epsilon( 1.0e-6 ) );
+            REQUIRE( arrivalVelocity[ i ] == Approx( velocities.second[ i ] ).epsilon( 1.0e-6 ) );
+        }
+
+        // Check that no iterations are required.
+        REQUIRE( numberOfIterations == 0 );
+    }
+}
+
 // This is a legacy test which should result in an exception being thrown since the tsince bug was fixed
 TEST_CASE( "Execute Legacy Atom solver Test", "[atom-solver]")
 {
